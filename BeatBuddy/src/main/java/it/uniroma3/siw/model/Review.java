6548bfa047e1fb9f7d9829bf0b439afae4e5b65e
LeoFabbri <leo.fab02@gmail.com>
2024-04-26 12:16:48 +0200

    Classi Model e rispettivi collegamenti

---
+++ BeatBuddy/src/main/java/it/uniroma3/siw/model/Review.java
@@ -0,0 +1,80 @@
+package it.uniroma3.siw.model;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+
+@Entity
+public class Review {
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+    private Integer stars;
+	private String comment;
+    private LocalDate pubblicationDate;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+		return this.id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+    public Integer getStars() {
+        return stars;
+    }
+
+    public void setStars(Integer stars) {
+        this.stars = stars;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDate getPubblicationDate() {
+        return pubblicationDate;
+    }
+
+    public void setPubblicationDate(LocalDate pubblicationDate) {
+        this.pubblicationDate = pubblicationDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+	@Override
+	public boolean equals(Object o) {
+		Review r = (Review)o;
+		return this.stars.equals(r.getStars()) && this.comment.equals(r.getComment()) && this.pubblicationDate.equals(r.getPubblicationDate());
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.stars.hashCode() + this.comment.hashCode() + this.pubblicationDate.hashCode();
+	}
+
+}
---
+++ BeatBuddy/src/main/java/it/uniroma3/siw/model/User.java
@@ -0,0 +1,100 @@
+package it.uniroma3.siw.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+
+@Entity
+public class User {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+    private String name;
+	private String surname;
+    private LocalDate dateOfBirth;
+    private Email email;
+    //private String pwd;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Playlist> playlistsCreated;
+
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews;
+
+    public Long getId() {
+		return this.id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public List<Playlist> getPlaylistsCreated() {
+        return playlistsCreated;
+    }
+
+    public void setPlaylistsCreated(List<Playlist> playlistsCreated) {
+        this.playlistsCreated = playlistsCreated;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+	
+	@Override
+	public boolean equals(Object o) {
+		User a = (User)o;
+		return this.email.equals(a.getEmail());
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.email.hashCode();
+	}    
+}
